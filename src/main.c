3ca619826b00cdb90863a2443d7dc2da06f607f0
Artur Maslobojev <arthurmasl@gmail.com>
2024-03-02 12:23:56 +0200

    init

---
+++ src/main.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <raylib.h>
+#include <raymath.h>
+
+int main() {
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  InitWindow(screenWidth, screenHeight, "ray3d");
+  SetWindowPosition(1000, 0);
+  SetTargetFPS(60);
+
+  walkD = LoadTexture("resources/orc/D_Walk.png");
+  walkU = LoadTexture("resources/orc/U_Walk.png");
+  walkS = LoadTexture("resources/orc/S_Walk.png");
+  attackU = LoadTexture("resources/orc/U_Attack.png");
+  attackD = LoadTexture("resources/orc/D_Attack.png");
+  attackS = LoadTexture("resources/orc/S_Attack.png");
+
+  Texture2D textures[] = {walkU, walkD, walkS, walkS, attackU, attackD, attackS, attackS};
+
+  Camera2D camera = {0};
+  camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
+  camera.zoom = 2.5f;
+
+  while (!WindowShouldClose()) {
+    // update
+    if (IsKeyDown(KEY_W)) state = WALK_U, dir = U, vel.y += SPEED;
+    if (IsKeyDown(KEY_S)) state = WALK_D, dir = D, vel.y -= SPEED;
+    if (IsKeyDown(KEY_D)) state = WALK_R, dir = R, vel.x -= SPEED;
+    if (IsKeyDown(KEY_A)) state = WALK_L, dir = L, vel.x += SPEED;
+
+    if (IsMouseButtonPressed(0)) {
+      if (dir == U) state = ATTACK_U;
+      if (dir == D) state = ATTACK_D;
+      if (dir == L) state = ATTACK_L;
+      if (dir == R) state = ATTACK_R;
+    }
+
+    vel = Vector2Multiply(vel, (Vector2){FRICTION, FRICTION});
+    pos = Vector2Add(pos, Vector2Scale(vel, GetFrameTime()));
+
+    // animation
+    orcTimer -= GetFrameTime();
+    if (orcTimer <= 0) orcTimer = ORC_TIME, orcIndex++;
+
+    // draw
+    BeginDrawing();
+    BeginMode2D(camera);
+    ClearBackground(RAYWHITE);
+
+    int scaleX = 1;
+    int scaleY = 1;
+    if (dir == L) scaleX = 1;
+    if (dir == R) scaleX = -1;
+
+    Rectangle source = {ORC_SIZE * orcIndex, 0, ORC_SIZE * scaleX, ORC_SIZE * scaleY};
+    Rectangle dest = {0, 0, source.width * scaleX, source.height * scaleY};
+    Vector2 origin = (Vector2){pos.x, pos.y};
+
+    DrawTexturePro(textures[state], source, dest, origin, 0, WHITE);
+
+    // DrawRectangle(0, 0, ORC_SIZE, ORC_SIZE, (Color){255, 0, 0, 50});
+    EndMode2D();
+
+    // UI
+    DrawFPS(10, 10);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,41 @@
+#include <raylib.h>
+#include <raymath.h>
+
+#define ORC_SIZE  48
+#define ORC_COUNT 6
+#define ORC_FPS   10
+#define ORC_TIME  1.0f / ORC_FPS
+
+#define SPEED     10.0f
+#define FRICTION  0.8f
+
+const int screenWidth = 680;
+const int screenHeight = 997;
+
+Texture2D walkD;
+Texture2D walkU;
+Texture2D walkS;
+Texture2D attackU;
+Texture2D attackD;
+Texture2D attackS;
+
+int orcIndex = 0;
+float orcTimer = ORC_TIME;
+
+Vector2 vel = {0, 0};
+Vector2 pos = {0, 0};
+
+enum states {
+  WALK_U,
+  WALK_D,
+  WALK_L,
+  WALK_R,
+  ATTACK_U,
+  ATTACK_D,
+  ATTACK_L,
+  ATTACK_R,
+};
+
+enum dirs { L, R, U, D };
+enum states state = WALK_D;
+enum dirs dir = L;
